15a79b1def3781041fbf919fc77c429ecaa61fd3
Mudasir Raza <mudasir.raza@arkhitech.com>
2015-05-15 16:38:35 +0500

    admin rating name modified for easy post

--- app/models/spree/shipping_rate_decorator.rb
+++ app/models/spree/shipping_rate_decorator.rb
@@ -2,4 +2,14 @@ Spree::ShippingRate.class_eval do
   def name
     read_attribute(:name) || shipping_method.name
   end
+
+  def admin_name
+    read_attribute(:name) || begin
+      if self.easy_post_rate_id.nil?
+        shipping_method.name
+      else
+        "EasyPost: #{shipping_method.name}"
+      end
+    end
+  end
 end
